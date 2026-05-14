c6cfa1d7a3d5b43b8fc57f25e1d4dc79ab0e5000
chachu <chachuarakkalshanavas@gmail.com>
2026-05-14 19:52:28 +0530

    feat: add GitHub Pages deployment workflow and update app routing to support subdirectory hosting

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import './index.css'
 
 // Redirect to home page on any reload, before React even mounts
 if (performance.getEntriesByType('navigation')[0]?.type === 'reload') {
-  if (window.location.pathname !== '/') {
-    window.location.replace('/')
+  if (window.location.pathname !== '/cytech/') {
+    window.location.replace('/cytech/')
   }
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cytech/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
