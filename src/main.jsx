754d6f3ce2055ff335b42c544127bfb71e4085ad
chachu <chachuarakkalshanavas@gmail.com>
2026-05-14 20:06:25 +0530

    chore: update base path to /cytech/ for deployment routing compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import './index.css'
 
 // Redirect to home page on any reload (Fix for SPA 404 on refresh)
 if (performance.getEntriesByType('navigation')[0]?.type === 'reload') {
-  if (window.location.pathname !== '/') {
-    window.location.replace('/')
+  if (window.location.pathname !== '/cytech/') {
+    window.location.replace('/cytech/')
   }
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cytech/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
